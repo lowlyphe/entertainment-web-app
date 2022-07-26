9965238449c5461d4cf3ad85590494d7fe32780b
lowlyphe <jones.kyle2893@gmail.com>
2022-07-26 13:05:23 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const $trendingContainer = $('.trending-container')
+const $trending = $('.trending')
+
+// $(document).ready(() => {
+//     $trendingContainer.empty()
+//     $.get('localhost:3000/index/trending-data', (data) => {
+//         console.log(data)
+//     })
+    
+// })
+
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        red: '#fc4747',
+        black: '#10141e',
+        grayishBlue: '#5a698f',
+        darkGray: '#161d2f',
+      },
+      fontFamily: {
+        'sans': "Outfit"
+      }
+    },
+  },
+  plugins: [],
+}
