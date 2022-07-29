f23a2f8238a1c7cc5c9222fbcb4ba0d0f1dd6dd3
lowlyphe <jones.kyle2893@gmail.com>
2022-07-29 16:34:52 -0500

    trying to get tailwind to work in prod

--- server.js
+++ server.js
@@ -17,23 +17,9 @@ const pool = new pg.Pool({
   ssl: NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 
-// app.get('/', (req,res) => {
-//     fs.readFile('./../client/index.html', 'utf-8', (data) => {
-//         console.log(data);
-//     })
-// })
-
-// app.get('/shows', (req,res) => {
-//     fs.readFile('data.json', 'utf-8', (data) => {
-//         return data;
-//     }).then((data) => {
-//         res.status(200).type('applicaiton/json').send(data)
-//     })
-// })
 
 // read data
 app.get('/api/trending', (req,res) => (
-    // res.set('Access-Control-Allow-Origin', '*').send('good')
     pool.query('SELECT * FROM movies where isTrending = true', ).then((data) => {
         res.status(200).type('application/json').send(data.rows)
     })
